cfe751538426761ddf95eec7d3617697caa69af5
Josh Holbrook <josh.holbrook@gmail.com>
2025-02-12 09:39:56 -0900

    level name is verbose

--- stardeck-playbook/logging.mjs
+++ stardeck-playbook/logging.mjs
@@ -6,18 +6,19 @@ const RESET = colorCode(0);
 
 export const LEVELS = {
   debug: { severity: 40, verbosity: 3, color: 35 },
-  verb: { severity: 30, verbosiry: 2, color: 36 },
+  verbose: { tag: 'verb', severity: 30, verbosiry: 2, color: 36 },
   info: { severity: 20, verbosity: 1, color: 32 },
   warn: { severity: 10, verbosity: 0, color: 33 },
   error: { severity: 0, verbosity: 0, color: 31 },
 };
 
-const ALIASES = {
-  verbose: 'verb',
+export const LEVEL_ALIASES = {
   warning: 'warn',
 };
 
-const MAX_LEN = Math.max(...Object.keys(LEVELS).map((lvl) => lvl.length));
+const MAX_LEN = Math.max(
+  ...Object.entries(LEVELS).map(([name, lvl]) => (lvl.tag || name).length),
+);
 
 for (let [name, lvl] of Object.entries(LEVELS)) {
   lvl.padding = MAX_LEN - name.length;
@@ -29,8 +30,9 @@ let logger = {
 
   log(level, message, ...optionalParams) {
     if (LEVELS[level].severity <= this._severity) {
+      const tag = LEVELS[level].tag || level;
       const color = colorCode(LEVELS[level].color);
-      let msg = `${color}${level}${RESET}`;
+      let msg = `${color}${tag}${RESET}`;
       msg = msg + ':' + ' '.repeat(LEVELS[level].padding);
 
       if (typeof message == 'string') {
@@ -52,7 +54,7 @@ for (let lvl of Object.keys(LEVELS)) {
   };
 }
 
-for (let [alias, to] of Object.entries(ALIASES)) {
+for (let [alias, to] of Object.entries(LEVEL_ALIASES)) {
   logger[alias] = logger[to];
 }
 
