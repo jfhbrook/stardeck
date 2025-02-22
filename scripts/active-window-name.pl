1564031fbc594ab13ed49a9cdbed9ca5a5d7ceb0
Josh Holbrook <josh.holbrook@gmail.com>
2025-02-21 15:20:30 -0900

    Get the active window name with kdotool

---
+++ scripts/active-window-name.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+
+use v5.40;
+use warnings;
+
+package main;
+
+my sub active_window_name {
+  my $active_window = `kdotool getactivewindow`;
+  `kdotool getwindowname $active_window`;
+}
+
+my $name = &active_window_name;
+
+say $name;
