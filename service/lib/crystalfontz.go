d93201a2fb1c0f0b1b753e80d4c89817a4eda988
Josh Holbrook <josh.holbrook@gmail.com>
2025-08-27 11:44:39 -0800

    plusdeck/crystalfontz signal constants

--- service/lib/crystalfontz.go
+++ service/lib/crystalfontz.go
@@ -4,6 +4,23 @@ import (
 	"github.com/godbus/dbus/v5"
 )
 
+type KeyActivity = byte
+
+const (
+	KeyUpPress      KeyActivity = 1
+	KeyDownPress                = 2
+	KeyLeftPress                = 3
+	KeyRightPress               = 4
+	KeyEnterPress               = 5
+	KeyExitPress                = 6
+	KeyUpRelease                = 7
+	KeyDownRelease              = 8
+	KeyLeftRelease              = 9
+	KeyRightRelease             = 10
+	KeyEnterRelease             = 11
+	KeyExitRelease              = 12
+)
+
 func AddCrystalfontzMatchSignal(conn *dbus.Conn) error {
 	return conn.AddMatchSignal(
 		dbus.WithMatchObjectPath("/"),
--- service/lib/plusdeck.go
+++ service/lib/plusdeck.go
@@ -4,6 +4,23 @@ import (
 	"github.com/godbus/dbus/v5"
 )
 
+type PlusdeckState = string
+
+const (
+	PlayingA        PlusdeckState = "PLAYING_A"
+	PausedA                       = "PAUSED_A"
+	PlayingB                      = "PLAYING_B"
+	Subscribed                    = "SUBSCRIBED"
+	PausedB                       = "PAUSED_B"
+	FastForwardingA               = "FAST_FORWARDING_A"
+	FastForwardingB               = "FAST_FORWARDING_B"
+	Stopped                       = "STOPPED"
+	Ejected                       = "EJECTED"
+	Subscribing                   = "SUBSCRIBING"
+	Unsubscribing                 = "UNSUBSCRIBING"
+	Unsubscribed                  = "UNSUBSCRIBED"
+)
+
 func AddPlusdeckMatchSignal(conn *dbus.Conn) error {
 	return conn.AddMatchSignal(
 		dbus.WithMatchObjectPath("/"),
