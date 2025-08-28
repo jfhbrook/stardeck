3b078d033afbc75f6c5e68fd72fdeabc8fd5b61b
Josh Holbrook <josh.holbrook@gmail.com>
2025-08-28 11:13:54 -0800

    switch on event type

--- service/lib/service.go
+++ service/lib/service.go
@@ -28,62 +28,16 @@ func Service() error {
 
 	for {
 		event := <-events
-		fmt.Println(event)
+
+		switch event.Type {
+		case WindowEvent:
+			fmt.Println(event)
+		case PlusdeckEvent:
+			fmt.Println(event)
+		case KeyActivityReport:
+			fmt.Println(event)
+		case Notification:
+			fmt.Println(event)
+		}
 	}
 }
-
-/*
-my %commands = (
-    StartProcess => sub {
-        my $name = $_->{'name'};
-        my $pid  = $_->{'pid'};
-
-        $pids{$name} = $pid;
-    },
-    ExitProcess => sub {
-
-        # Clean up the PID
-        my $name = $_->{'name'};
-        delete $pids{$name};
-
-        # A process exiting is unexpected, so we need to error out. Still,
-        # we can killl the other processes gracefully.
-        &graceful_exit(1);
-
-    },
-    ActiveWindow => sub {
-        print Dumper($_);
-    },
-    PlusdeckState => sub {
-        print Dumper($_);
-    },
-    KeyActivityReport => sub {
-        print Dumper($_);
-    },
-    TemperatureReport => sub {
-        print Dumper($_);
-    },
-    Notification => sub {
-        print Dumper($_);
-    }
-);
-
-my sub command_worker {
-    while ( my $event = $main_commands->dequeue() ) {
-        my $type    = $event->{'type'};
-        my $handler = $commands{$type};
-
-        if ($handler) {
-            $handler->($event);
-        }
-        else {
-            print Dumper($event);
-        }
-    }
-
-    say "Unreachable.";
-    &hard_exit(1);
-
-    return;
-}
-*/
