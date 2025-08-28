b8ecc5c2a56e566157478ce8eb823aee22ab5819
Josh Holbrook <josh.holbrook@gmail.com>
2025-08-28 12:12:06 -0800

    loopback manager

--- service/lib/plusdeck.go
+++ service/lib/plusdeck.go
@@ -1,7 +1,9 @@
 package lib
 
 import (
+	"fmt"
 	"github.com/godbus/dbus/v5"
+	"os/exec"
 )
 
 type PlusdeckState = string
@@ -36,4 +38,63 @@ func newPlusdeckEvent(state string) *Event {
 
 func HandlePlusdeckState(signal *dbus.Signal, events *chan *Event) {
 	*events <- newPlusdeckEvent(signal.Body[0].(string))
+}
+
+const (
+	DefaultLoopbackSource  string = "alsa_input.pci-0000_00_1f.3.analog-stereo"
+	DefaultLoopbackLatency int32  = 1
+	DefaultLoopbackVolume  int32  = 10
+)
+
+type LoopbackManager struct {
+	source  string
+	latency int32
+	volume  int32
+}
+
+func NewLoopbackManager(source string, latency int32, volume int32) *LoopbackManager {
+	src := source
+	if src == "" {
+		src = DefaultLoopbackSource
+	}
+
+	lt := latency
+	if lt < 0 {
+		lt = DefaultLoopbackLatency
+	}
+
+	vol := volume
+	if vol < 0 {
+		vol = DefaultLoopbackVolume
+	}
+
+	lb := LoopbackManager{source: src, latency: lt, volume: vol}
+
+	return &lb
+}
+
+func (lb *LoopbackManager) Enable() error {
+	if err := exec.Command(
+		"pactl",
+		"load-module",
+		"module-loopback",
+		fmt.Sprintf("--latency_msec=%d", lb.latency),
+	).Run(); err != nil {
+		return err
+	}
+
+	return exec.Command(
+		"pactl",
+		"set-source-volume",
+		lb.source,
+		fmt.Sprintf("%d", lb.volume),
+	).Run()
+}
+
+func (lb *LoopbackManager) Disable() error {
+	return exec.Command(
+		"pactl",
+		"unload-module",
+		"module-loopback",
+	).Run()
 }
