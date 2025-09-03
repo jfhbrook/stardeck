eb0bde22bd70d6709d46fb574872fdc2b766106f
Josh Holbrook <josh.holbrook@gmail.com>
2025-09-03 09:09:41 -0800

    Final touches on upgrade

--- scripts/cleanup-extras.sh
+++ scripts/cleanup-extras.sh
@@ -6,16 +6,18 @@
 #     https://docs.fedoraproject.org/en-US/quick-docs/upgrading-fedora-offline/
 #
 
-sudo dnf list --extras
+sudo dnf list --extras | grep -v watchexec-cli
 
 read -p 'Would you like to remove extras? [y/N]' -n 1 -r
+echo
 if [[ "${REPLY}" =~ ^[Yy]$ ]]; then
   # shellcheck disable=SC2046
-  sudo dnf remove $(sudo dnf repoquery --extras --exclude=kernel,kernel-\*,kmod-\*)
+  sudo dnf remove $(sudo dnf repoquery --extras --exclude=kernel,kernel-\*,kmod-\*,watchexec-cli)
   sudo dnf autoremove
 else
   # shellcheck disable=SC2016
-  echo 'To remove all extras, run:
+  echo '
+To remove all extras, run:
     sudo dnf remove $(sudo dnf repoquery --extras --exclude=kernel,kernel-\*,kmod-\*)
 '
 fi
--- scripts/cleanup-symlinks.sh
+++ scripts/cleanup-symlinks.sh
@@ -2,11 +2,14 @@
 
 sudo symlinks -r /usr/ | grep dangling
 
+echo
 read -p 'Would you like to remove dangling symlinks? [y/N]' -n 1 -r
+echo
 if [[ "${REPLY}" =~ ^[Yy]$ ]]; then
   sudo symlinks -r -d /usr
 else
-  echo 'To remove all dangling symlinks, run:
+  echo '
+To remove all dangling symlinks, run:
     sudo symlinks -r -d /usr
 '
 fi
