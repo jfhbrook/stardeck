7c3a8f63077e07a5870cef7fd1f4dda83ce89e17
Josh Holbrook <josh.holbrook@gmail.com>
2025-09-04 07:48:15 -0800

    interval in kwin plugin

--- kwin/contents/code/main.js
+++ kwin/contents/code/main.js
@@ -1,7 +1,23 @@
-callDBus(
-  'org.jfhbrook.stardeck',
-  '/',
-  'org.jfhbrook.stardeck',
-  'setWindowName',
-  workspace.activeWindow.caption
-);
+const SECOND = 1000;
+
+const DBUS_DESTINATION = 'org.jfhbrook.stardeck';
+const DBUS_PATH = '/';
+const DBUS_INTERFACE = 'org.jfhbrook.stardeck';
+
+const WINDOW_INTERVAL = 1 * SECOND;
+
+function callback(methodName, ...args) {
+  callDBus(
+    DBUS_DESTINATION,
+    DBUS_PATH,
+    DBUS_INTERFACE,
+    methodName,
+    ...args
+  );
+}
+
+function setWindow() {
+  callback('setWindow', workspace.activeWindow.caption);
+}
+
+setInterval(setWindow, WINDOW_INTERVAL);
