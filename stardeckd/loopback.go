4ef1ebe956665fdb7348bdf9ea1a5d9d41ba7b2f
Josh Holbrook <josh.holbrook@gmail.com>
2025-09-09 09:28:21 -0800

    Split out loopback

---
+++ stardeckd/loopback.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+const (
+	DefaultLoopbackSource  string = "alsa_input.pci-0000_00_1f.3.analog-stereo"
+	DefaultLoopbackLatency int32  = 1
+	DefaultLoopbackVolume  int32  = 10
+)
+
+type LoopbackManager struct {
+	source  string
+	latency int32
+	volume  int32
+}
+
+func NewLoopbackManager(source string, latency int32, volume int32) *LoopbackManager {
+	src := source
+	if src == "" {
+		src = DefaultLoopbackSource
+	}
+
+	lt := latency
+	if lt < 0 {
+		lt = DefaultLoopbackLatency
+	}
+
+	vol := volume
+	if vol < 0 {
+		vol = DefaultLoopbackVolume
+	}
+
+	lb := LoopbackManager{source: src, latency: lt, volume: vol}
+
+	return &lb
+}
+
+func (lb *LoopbackManager) Enable() error {
+	if err := exec.Command(
+		"pactl",
+		"load-module",
+		"module-loopback",
+		fmt.Sprintf("--latency_msec=%d", lb.latency),
+	).Run(); err != nil {
+		return err
+	}
+
+	return exec.Command(
+		"pactl",
+		"set-source-volume",
+		lb.source,
+		fmt.Sprintf("%d", lb.volume),
+	).Run()
+}
+
+func (lb *LoopbackManager) Disable() error {
+	return exec.Command(
+		"pactl",
+		"unload-module",
+		"module-loopback",
+	).Run()
+}
--- stardeckd/plusdeck.go
+++ stardeckd/plusdeck.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"github.com/godbus/dbus/v5"
-	"os/exec"
 )
 
 type PlusdeckState = string
@@ -26,7 +24,7 @@ const (
 func AddPlusdeckMatchSignal(conn *dbus.Conn) error {
 	return conn.AddMatchSignal(
 		dbus.WithMatchObjectPath("/"),
-		dbus.WithMatchInterface("org.jfhbrook.crystalfontz"),
+		dbus.WithMatchInterface("org.jfhbrook.plusdeck"),
 	)
 }
 
@@ -38,63 +36,4 @@ func newPlusdeckEvent(state string) *Event {
 
 func HandlePlusdeckState(signal *dbus.Signal, events chan *Event) {
 	events <- newPlusdeckEvent(signal.Body[0].(string))
-}
-
-const (
-	DefaultLoopbackSource  string = "alsa_input.pci-0000_00_1f.3.analog-stereo"
-	DefaultLoopbackLatency int32  = 1
-	DefaultLoopbackVolume  int32  = 10
-)
-
-type LoopbackManager struct {
-	source  string
-	latency int32
-	volume  int32
-}
-
-func NewLoopbackManager(source string, latency int32, volume int32) *LoopbackManager {
-	src := source
-	if src == "" {
-		src = DefaultLoopbackSource
-	}
-
-	lt := latency
-	if lt < 0 {
-		lt = DefaultLoopbackLatency
-	}
-
-	vol := volume
-	if vol < 0 {
-		vol = DefaultLoopbackVolume
-	}
-
-	lb := LoopbackManager{source: src, latency: lt, volume: vol}
-
-	return &lb
-}
-
-func (lb *LoopbackManager) Enable() error {
-	if err := exec.Command(
-		"pactl",
-		"load-module",
-		"module-loopback",
-		fmt.Sprintf("--latency_msec=%d", lb.latency),
-	).Run(); err != nil {
-		return err
-	}
-
-	return exec.Command(
-		"pactl",
-		"set-source-volume",
-		lb.source,
-		fmt.Sprintf("%d", lb.volume),
-	).Run()
-}
-
-func (lb *LoopbackManager) Disable() error {
-	return exec.Command(
-		"pactl",
-		"unload-module",
-		"module-loopback",
-	).Run()
 }
