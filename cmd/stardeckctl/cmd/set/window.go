a23d942e121a4a92b1eb53eba3502df30b17c323
Josh Holbrook <josh.holbrook@gmail.com>
2025-09-12 07:14:23 -0800

    Use cobra exact args

--- cmd/stardeckctl/cmd/set/window.go
+++ cmd/stardeckctl/cmd/set/window.go
@@ -1,9 +1,7 @@
 package set
 
 import (
-	"errors"
-
-	pkgerrors "github.com/pkg/errors"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"github.com/godbus/dbus/v5"
 
@@ -16,17 +14,14 @@ var setWindowCmd = &cobra.Command{
 	Short: "Set the window title",
 	Long: `Set the title of the currently active window. This will be displayed on
 the LCD.`,
+  Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) != 1 {
-			logger.FlagrantError(errors.New("Window name is required"))
-		}
-
 		windowName := args[0]
 
 		conn, err := dbus.ConnectSessionBus()
 
 		if err != nil {
-		  logger.FlagrantError(pkgerrors.Wrap(err, "Failed to connect to session bus"))
+		  logger.FlagrantError(errors.Wrap(err, "Failed to connect to session bus"))
 		}
 
 		defer conn.Close()
