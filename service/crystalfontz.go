2690ffab11ad19f68e333501243cf20f28bc967a
Josh Holbrook <josh.holbrook@gmail.com>
2025-09-20 09:32:53 -0800

    Handle empty data for LCD

--- service/crystalfontz.go
+++ service/crystalfontz.go
@@ -1,7 +1,10 @@
 package service
 
 import (
+	"bytes"
+
 	"github.com/godbus/dbus/v5"
+	"github.com/pkg/errors"
 	"github.com/rs/zerolog/log"
 
 	"github.com/jfhbrook/stardeck/crystalfontz"
@@ -19,15 +22,24 @@ func makeCrystalfontzSender(systemConn *dbus.Conn) crystalfontzSender {
 		data := []byte(text)
 		if len(data) > 16 {
 			data = data[0:16]
+		} else {
+			data = append(data, bytes.Repeat([]byte{' '}, 16 - len(data))...)
 		}
+
+		log.Debug().
+			Int("row", int(row)).
+			Int("column", int(column)).
+			Str("data", string(data)).
+			Msg("Sending data to LCD")
 
 		err := lcd.SendData(row, column, data, -1.0, -1)
 
 		if err != nil {
-			log.Warn().
+			log.Error().
 				Int("row", int(row)).
 				Int("column", int(column)).
-				Err(err).
+				Str("data", string(data)).
+				Err(errors.Wrap(err, err.Error())).
 				Msg("Error while sending data to LCD")
 		}
 	}
