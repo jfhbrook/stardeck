4080dcd794cf2bb4e1b0db051e370e63bd69024b
Josh Holbrook <josh.holbrook@gmail.com>
2025-09-30 08:44:12 -0800

    stardeckd shenanigans

--- scripts/stardeckd.sh
+++ scripts/stardeckd.sh
@@ -4,4 +4,4 @@ STARDECK_HOME="$(realpath "$(dirname "$(dirname "${BASH_SOURCE[0]}")")")"
 
 cd "${STARDECK_HOME}" || exit 1
 
-exec go run ./cmd/stardeckd/main.go "$@"
+exec go run ./cmd/stardeckd/main.go "$@" 2>&1
