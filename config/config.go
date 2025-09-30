58033458f28cbd6c37a4966c6a400a647bee57f0
Josh Holbrook <josh.holbrook@gmail.com>
2025-09-30 09:07:00 -0800

    Explicitly bind env vars

--- config/config.go
+++ config/config.go
@@ -48,25 +48,41 @@ func InitConfig(cfgFile string, appType int) {
 	defaultPause := 3.0
 
 	viper.SetDefault("cli.log_level", defaultLogLevel)
+	viper.BindEnv("cli.log_level", "STARDECK_CLI_LOG_LEVEL")
 	viper.SetDefault("cli.log_format", logger.PrettyFormat)
+	viper.BindEnv("cli.log_format", "STARDECK_CLI_LOG_FORMAT")
 	viper.SetDefault("cli.log_color", defaultLogColor)
+	viper.BindEnv("cli.log_color", "STARDECK_CLI_LOG_COLOR")
 
 	viper.SetDefault("service.log_level", defaultLogLevel)
+	viper.BindEnv("service.log_level", "STARDECK_SERVICE_LOG_LEVEL")
 	viper.SetDefault("service.log_format", logger.JsonFormat)
+	viper.BindEnv("service.log_format", "STARDECK_SERVICE_LOG_FORMAT")
 	viper.SetDefault("service.log_color", defaultLogColor)
+	viper.BindEnv("service.log_color", "STARDECK_SERVICE_LOG_COLOR")
 
 	viper.SetDefault("crystalfontz.width", 16)
+	viper.BindEnv("crystalfontz.width", "STARDECK_CRYSTALFONTZ_WIDTH")
 	viper.SetDefault("crystalfontz.pause", defaultPause)
+	viper.BindEnv("crystalfontz.pause", "STARDECK_CRYSTALFONTZ_PAUSE")
 	viper.SetDefault("crystalfontz.tick", 0.3)
+	viper.BindEnv("crystalfontz.tick", "STARDECK_CRYSTALFONTZ_TICK")
 
 	viper.SetDefault("loopback.managed", true)
+	viper.BindEnv("loopback.managed", "STARDECK_LOOPBACK_MANAGED")
 	viper.SetDefault("loopback.source", "alsa_input.pci-0000_00_1f.3.analog-stereo")
+	viper.BindEnv("loopback.source", "STARDECK_LOOPBACK_SOURCE")
 	viper.SetDefault("loopback.latency", 1)
+	viper.BindEnv("loopback.latency", "STARDECK_LOOPBACK_LATENCY")
 	viper.SetDefault("loopback.volume", 10)
+	viper.BindEnv("loopback.volume", "STARDECK_LOOPBACK_VOLUME")
 
 	viper.SetDefault("notifications.timeout", 15.0)
+	viper.BindEnv("notifications.timeout", "STARDECK_NOTIFICATIONS_TIMEOUT")
 	viper.SetDefault("notifications.min_wait", defaultPause)
+	viper.BindEnv("notifications.min_wait", "STARDECK_NOTIFICATIONS_MIN_WAIT")
 	viper.SetDefault("notifications.max_queue_length", 5)
+	viper.BindEnv("notifications.max_queue_length", "STARDECK_NOTIFICATIONS_MAX_QUEUE_LENGTH")
 
 	if cfgFile != "" {
 		viper.SetConfigFile(cfgFile)
